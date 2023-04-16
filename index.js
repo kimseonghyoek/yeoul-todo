6846a90a5cf432c3b63fc039044e47c3f18a8060
overloper_mac <overloper@overloper-macui-noteubug.local>
2023-04-16 16:44:57 +0900

    Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var dates = document.getElementById("time");
 var userName = document.getElementById("user-name");
 var todolist = document.getElementById("todo-list");
+var todoForm = document.getElementById("add-todo");
+var todoInput = document.getElementById("todo-inputs")
 
 function Clock() {
   var date = new Date();
@@ -11,24 +13,37 @@ function Clock() {
 
   dates.innerText = `${hours}:${min < 10 ? `0${min
   }` : `${min}`}:${sec < 10 ? `0${sec}` : `${sec}`}`;
-}
+};
 
 function init() {
   Clock();
   setInterval(Clock, 1000);
-}
+};
 
 function DrawName() {
   var name = localStorage.getItem("User");
   userName.innerText = `Welcome, ${name}`
-}
+};
 
 function AddTodo(todo) {
   var li = document.createElement('li');
   var span = document.createElement("span");
   var p = document.createElement("p");
   var btn = document.createElement("button");
-}
+
+  btn.innerText = "❌";
+};
+
+function HandleSubmit(event) {
+  event.preventDefault();
+  value = todoInput.value;
+};
+
+function Form() {
+  todoForm.addEventListener("submit", HandleSubmit);
+};
+
+Form();
 
 DrawName();
 init();
