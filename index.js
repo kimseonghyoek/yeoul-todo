0f0e36d8f92c8593108b47475f0cff7a52c74141
overloper <overloper0225@gmail.com>
2023-04-18 15:47:19 +0900

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var dates = document.getElementById("time");
 var userName = document.getElementById("user-name");
 var todoList = document.getElementById("todo-list");
 var todoForm = document.getElementById("add-todo");
-var todoInput = document.getElementById("todo-inputs")
+var todoInput = document.getElementById("todo-inputs");
 
 function Clock() {
   var date = new Date();
@@ -31,12 +31,27 @@ function AddTodo(todo) {
   var p = document.createElement("p");
   var btn = document.createElement("button");
 
+  li.id = todo.id;
+  p.innerText = todo.text;
   btn.innerText = "❌";
+  li.appendChild(span);
+  span.appendChild(p);
+  span.appendChild(btn);
+  todoList.appendChild(li);
 };
 
 function HandleSubmit(event) {
   event.preventDefault();
   value = todoInput.value;
+
+  const newTodoObj = {
+    text: value,
+    id: 1
+  }
+
+  localStorage.setItem("todo-list", newTodoObj.text);
+  AddTodo(newTodoObj);
+  todoInput.value = "";
 };
 
 function Form() {
@@ -45,5 +60,19 @@ function Form() {
 
 Form();
 
+function getTodo() {
+  const newTodoObj = {
+    text: value,
+    id: 1
+  }
+  localStorage.getItem("todo-list");
+  AddTodo(newTodoObj); 
+}
+
+function delTodo(todoid) {
+  // document.getElementById(todoid.id)
+}
+
 DrawName();
 init();
+getTodo();
